805a11b7c45fd45e0ef4f2900d429de0320509d8
애니닷 <whiteratsjh@gmail.com>
2019-02-21 15:17:07 +0900

    Signed-off-by: 애니닷 <whiteratsjh@gmail.com>

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 
 app.use(express.static('public'));
@@ -7,8 +8,18 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-app.get('/express/:page', (req,res) => {
-   res.render(`${req.path.replace("/","")}.html`);
+app.get("/", (req,res) => {
+   res.render("index.html");
+});
+
+app.get('/example/:page', (req,res) => {
+   res.render(`${req.params.page}.html`);
+});
+
+app.get('/files', (req,res) => {
+   fs.readdir("./views/", (err, files)=>{
+       res.send(files);
+   })
 });
 
 app.listen(3000, function () {
